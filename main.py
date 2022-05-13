cf9313c1cae8a9e66a73d81ed567b46c622d1787
Arnaldo Mont'Alvao <almontalvao#gmail.com>
2022-05-12 22:20:15 -0500

    Added Hugginface model and created post endpoint.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
+
+class PredictionRequest(BaseModel):
+  query_string: str
 
 @app.get("/health")
 def health():
     return "Service is online."
 
-    
+@app.post("my-endpoint")
+def my_endpoint(request: PredictionRequest):
+    sentiment_model = pipeline("sentiment-analysis")
+    sentiment_query_sentence = get_random_comment(top_comments)
+    sentiment = sentiment_model(sentiment_query_sentence)
+    return print(f"Sentiment test: {sentiment_query_sentence} == {sentiment}")
