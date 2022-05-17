fc5a6aec36009fff1b04a400c6131ad5db1a6edd
Arnaldo Mont'Alvao <almontalvao#gmail.com>
2022-05-16 22:09:59 -0500

    Updating main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
+import uvicorn
 
 app = FastAPI()
+
+sentiment_model = pipeline("sentiment-analysis")
+
+def sentiment_prediction(sentiment_query_sentence):
+  sentiment = sentiment_model(sentiment_query_sentence)
+  print(f"Sentiment test: {sentiment_query_sentence} == {sentiment}")
+  return sentiment
 
 class PredictionRequest(BaseModel):
   query_string: str
@@ -11,9 +19,9 @@ class PredictionRequest(BaseModel):
 def health():
     return "Service is online."
 
-@app.post("my-endpoint")
+@app.post("/predict_sentiment")
 def my_endpoint(request: PredictionRequest):
-    sentiment_model = pipeline("sentiment-analysis")
-    sentiment_query_sentence = get_random_comment(top_comments)
-    sentiment = sentiment_model(sentiment_query_sentence)
-    return print(f"Sentiment test: {sentiment_query_sentence} == {sentiment}")
+  return sentiment_prediction(request.query_string)
+ 
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0')
